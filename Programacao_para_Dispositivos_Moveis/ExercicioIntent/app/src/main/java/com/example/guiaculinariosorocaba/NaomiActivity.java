d8662b52a4c87205a5e11dd16b9a371703739b0b
Evelyn-RR <64284758+Evelyn-RR@users.noreply.github.com>
2022-04-25 17:58:22 -0300

    Fazer o backend

---
+++ Programacao_para_Dispositivos_Moveis/ExercicioIntent/app/src/main/java/com/example/guiaculinariosorocaba/NaomiActivity.java
@@ -0,0 +1,14 @@
+package com.example.guiaculinariosorocaba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NaomiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_naomi);
+    }
+}
